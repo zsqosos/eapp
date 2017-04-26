e1a2fa072236f9412fbcb63e1c451a7e3d701157
MEMEME <zsqos@hotmail.com>
2017-04-26 23:26:41 +0800

    header components completed

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,19 +29,40 @@
     <div class="background">
       <img :src="headerSeller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-content">
-          <h1 class="name">{{headerSeller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="headerSeller.score"></star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-content">
+            <h1 class="name">{{headerSeller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="headerSeller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="headerSeller.supports" class="supports">
+              <li class="supports-item" v-for="(item,index) in headerSeller.supports">
+                <span class="icon" :class="classMap[headerSeller.supports[index].type]"></span>
+                <span class="text">{{headerSeller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div v-if="headerSeller.bulletin"class="bulletin">
+              <p class="content">{{headerSeller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div @click="closeDetail" class="detail-close">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div @click="closeDetail" class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -201,6 +222,13 @@ export default {
       z-index: 50
       overflow: auto
       background: rgba(7,17,27,0.8)
+      transition: all 0.5s
+      // &.fade-enter-active, &.fade-leave-active
+      //   opacity: 1
+      //   background: rgba(7,17,27,0.8)
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+        background: rgba(7,17,27,0)
       .detail-wrapper
         min-height: 100%
         width: 100%
@@ -216,6 +244,58 @@ export default {
             margin-top: 18px
             padding: 2px 0
             text-align: center
+          .title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255,255,255,0.2)
+            .text
+              padding: 0 12px
+              font-size: 14px
+              font-weight: 700
+          .supports
+            margin: 0 auto
+            width: 80%
+            .supports-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                margin-right: 16px
+                vertical-align: top
+                background-size: 16px 16px
+                backgroun-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                font-size: 12px
+                line-height: 16px
+                font-weight: 200
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              margin-bottom: 16px
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
       .detail-close
         position: relative
         width: 32px
